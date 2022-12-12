55c1f057e34f844eacdfb498a36d5fefc5ffb0d2
Jonahas <jonas.h.faber@gmail.com>
2022-12-12 22:00:14 +0100

    prevent api deployment to be dependent on

--- steps/03_Deployment.py
+++ steps/03_Deployment.py
@@ -12,56 +12,21 @@ from dotenv import load_dotenv
 # This line read those in.
 load_dotenv()
 
-LABELS = os.environ.get('LABELS').split(',')
 MODEL_NAME = os.environ.get('MODEL_NAME')
 LOCAL_DEPLOYMENT = os.environ.get('LOCAL_DEPLOYMENT')
-
-
-def prepareEnv(ws):
-    print('Preparing environment')
-    environment_name = os.environ.get('DEPLOYMENT_ENV_NAME')
-    conda_dependencies_path = os.environ.get('DEPLOYMENT_DEPENDENCIES')
-
-    env = Environment.from_conda_specification(environment_name, file_path=conda_dependencies_path)
-    # Register environment to re-use later
-    env.register(workspace = ws)
-
-    return env
-
-def prepareDeployment(ws, environment):
-    print('Preparing deployment')
-    service_name = os.environ.get('SCORE_SERVICE_NAME')
-    entry_script = os.path.join(os.environ.get('SCRIPT_FOLDER'), 'score.py')
-
-    inference_config = InferenceConfig(entry_script=entry_script, environment=environment)
-    aci_config = AciWebservice.deploy_configuration(cpu_cores=1, memory_gb=1)
-
-    # Get our model based on the name we registered in the previous notebook
-    model = Model(ws, MODEL_NAME)
-
-    service = Model.deploy(workspace=ws,
-                        name=service_name,
-                        models=[model],
-                        inference_config=inference_config,
-                        deployment_config=aci_config,
-                        overwrite=True)
-    return service
 
 def downloadLatestModel(ws):
     print('Downloading latest model')
     local_model_path = os.environ.get('LOCAL_MODEL_PATH')
     model = Model(ws, name=MODEL_NAME)
-    downloaded_path = model.download(local_model_path, exist_ok=True)
+    downloaded_path = model.download(local_model_path, exist_ok=False)
     return downloaded_path
-
 
 def main():
     ws = connectWithAzure()
 
     downloadLatestModel(ws)
     print('done downloading model')
-
-    os.close(0)
 
     # environment = prepareEnv(ws)
     # service = prepareDeployment(ws, environment)
