f3835c6607bda7e38ce6787b8aad5bbd9e04eae7
Jonahas <jonas.h.faber@gmail.com>
2022-12-12 09:38:48 +0100

    fix: get dataset by name instead of all

--- steps/02_Training.py
+++ steps/02_Training.py
@@ -102,11 +102,10 @@ def prepareTraining(ws, env, compute_target) -> Tuple[Experiment, ScriptRunConfi
     datasets = Dataset.get_all(workspace=ws) # Get all the datasets
     exp = Experiment(workspace=ws, name=experiment_name) # Create a new experiment
 
-    print(datasets, train_set_name)
 
     args = [
         # You can set these to .as_mount() when not training on local machines, but this should also work.
-    '--training-folder', datasets[train_set_name].as_download('./data/train'), # Currently, this will always take the last version. You can search a way to specify a version if you want to
+    '--training-folder', Dataset.get_by_name(name=train_set_name, workspace=ws).as_download('./data/train'), # Currently, this will always take the last version. You can search a way to specify a version if you want to
     '--testing-folder', datasets[test_set_name].as_download('./data/test'), # Currently, this will always take the last version. You can search a way to specify a version if you want to
     '--epochs', MAX_EPOCHS,
     '--seed', SEED,
