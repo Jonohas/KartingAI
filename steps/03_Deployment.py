daf4e6d360f7949df2cdca710c0ff83470ba734c
Jonahas <jonas.h.faber@gmail.com>
2022-12-12 21:29:05 +0100

    test: downloadModel doesnt stop

--- steps/03_Deployment.py
+++ steps/03_Deployment.py
@@ -51,8 +51,8 @@ def downloadLatestModel(ws):
     print('Downloading latest model')
     local_model_path = os.environ.get('LOCAL_MODEL_PATH')
     model = Model(ws, name=MODEL_NAME)
-    model.download(local_model_path, exist_ok=True)
-    return
+    downloaded_path = model.download(local_model_path, exist_ok=True)
+    return downloaded_path
 
 
 def main():
